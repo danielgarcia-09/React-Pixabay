1b8fca03d4a765e3aa229b3ec66398bae54c39aa
Daniel Garcia <daniel.garcia7913@gmail.com>
2021-10-09 19:26:30 -0400

    NEW: error component and form validated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
 import Form from "./components/Form";
 
 function App() {
+
+  //* App state
+  const [ search, searching ] = useState('');
+
   return (
       <div className="container">
         <div className="jumbotron">
           <p className="lead text-center">Buscador de imagenes</p>
-          <Form />
+          <Form 
+              searching={searching}
+          />
         </div>
       </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,38 @@
-const Form = () => {
+import { useState } from 'react';
+import Error from './Error';
+
+const Form = ({searching}) => {
+
+    const [ searchValue, updateSearchValue ] = useState('');
+    
+    const [error, updateError] = useState(false);
+
+    const searchImages = e => {
+        e.preventDefault();
+
+        //* validate
+        if( searchValue.trim() === '' ) {
+            updateError(true);
+            return;
+        }
+
+        updateError(false);
+
+        //* send search value to app component
+        searching(searchValue);
+    }
+
     return (
-        <form>
+        <form
+            onSubmit={ searchImages }
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen, ejemplo: futbol o cafe"
+                        onChange={ e => updateSearchValue( e.target.value ) }
                     /> 
                 </div>
                 <div className="form-group col-md-4">
@@ -17,13 +43,10 @@ const Form = () => {
                     /> 
                 </div>
             </div>
+
+            { error ? <Error message="Agrega un termino de busqueda" /> : null}
         </form>
     );
 }
  
 export default Form;
-<form>
-    <div className="row">
-        <div className="form-group col-md-8">
-    </div>
-</form>
